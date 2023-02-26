462e54bffe62ff8f4f3936f1504b3cc09c4729fc
sahil <tiwarisahil585@gmail.com>
2023-02-27 03:21:53 +0530

    initial commit

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
 	"./run"
 )
 
 func main() {
+
 	http.HandleFunc("/submit", run.Submit)
 	log.Println("Listeing on :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- run/run.go
+++ run/run.go
@@ -9,31 +9,31 @@ func RunCode(submissionId, problemId, input_dir string) {
 
 	submission_dir := submissionId
 
-	compiled := Compile(submission_dir+"/"+submissionId+".cpp" , submission_dir+"/exec")
-	if(!compiled) {
+	compiled := Compile(submission_dir+"/"+submissionId+".cpp", submission_dir+"/exec")
+	if !compiled {
 		println("Compilation Error")
 		return
 	} else {
 		println("Compilation Success")
 	}
-	
+
 	output_dir := submissionId + "/output"
 	error_dir := submissionId + "/error"
 	meta_dir := submissionId + "/meta"
 
-	err := os.Mkdir(output_dir, 0777);
+	err := os.Mkdir(output_dir, 0777)
 	if err != nil {
 		os.RemoveAll(submission_dir)
 		return
 	}
 
-	err = os.Mkdir(error_dir, 0777);
+	err = os.Mkdir(error_dir, 0777)
 	if err != nil {
 		os.RemoveAll(submission_dir)
 		return
 	}
 
-	err = os.Mkdir(meta_dir, 0777);
+	err = os.Mkdir(meta_dir, 0777)
 	if err != nil {
 		os.RemoveAll(submission_dir)
 		return
@@ -46,9 +46,8 @@ func RunCode(submissionId, problemId, input_dir string) {
 		panic(err)
 	}
 
-
 	// // time docker run -e SUBMISSION_ID="1" - TIME_LIMIT=2 -e MEMORY_LIMIT=100 -v /home/sahil/dockerTest/cpp:/1 my-image
-	dir,_ := os.Getwd()
+	dir, _ := os.Getwd()
 	err = exec.Command("docker", "run", "-e", "SUBMISSION_ID="+submissionId, "-e", "TIME_LIMIT=2", "-e", "MEMORY_LIMIT=100", "-v", dir+"/"+submission_dir+":/"+submissionId, "my-image").Run()
 	if err != nil {
 		return
